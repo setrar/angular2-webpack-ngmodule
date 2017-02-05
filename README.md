2faf68660d8be7f81c16b6c5967a65b0f786a543
Setra <setrar@valiha.com>
2017-02-05 15:51:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # angular2-webpack-ngmodule
 Angular2, Webpack and Live NgModule example
+
+
+https://angular.io/docs/ts/latest/guide/webpack.html
+
+https://angular.io/docs/ts/latest/guide/ngmodule.html
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { CrisisCenterModule }   from './crisis-center/crisis-center.module';
+import { HeroesModule }         from './heroes/heroes.module';
+import { LoginRoutingModule }   from './login/login-routing.module';
+import { LoginComponent }       from './login/login.component';
+
+import { DialogService }        from './shared/dialog.service';
+
+import '../styles.css'
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroesModule,
+    LoginRoutingModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LoginComponent
+  ],
+  providers: [
+    DialogService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
