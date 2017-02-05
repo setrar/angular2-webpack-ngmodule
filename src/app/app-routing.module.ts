b3d29396d36564422024014ce2cad260ff38623f
setrar <setrar@valiha.com>
2017-02-05 15:53:13 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,6 @@ import { AuthGuardService }                from './login/shared/auth-guard.servi
 import { CanDeactivateGuardService }       from './login/shared/can-deactivate-guard.service';
 import { SelectivePreloadStrategyService } from './shared/selective-preload-strategy.service';
 
-const adminModulePath  = 'app/admin/admin.module#AdminModule';
-const crisisModulePath = 'app/crisis-center/crisis-center.module#CrisisCenterModule';
-
 const appRoutes: Routes = [
   {
     path: 'admin',
